b7355454b0bce00bdb9089501430c35bc27b4ae5
sanaa <Sanaakumar07@gmail.com>
2025-09-13 13:10:34 +0530

    check types of traingle

--- triangle/main.c
+++ triangle/main.c
@@ -1,14 +1,15 @@
-//
-//  main.c
-//  triangle
-//
-//  Created by Sanaa Kumar on 13/09/25.
-//
-
 #include <stdio.h>
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int s1,s2,s3;
+    printf("enter the valus of the sides =");
+    scanf("%d %d %d",&s1,&s2,&s3);
+    if ((s1==s2)  && (s2==s3) && (s3=s1)){
+        printf("equilateral traingle");
+    }
+   else if ((s1==s2 || s1==s3 || s2==s3) && s1!=s2 != s3 ){
+        printf("isosceles triangle ");
+    }
+    else
+        printf("scalene triangle");
     return 0;
 }
