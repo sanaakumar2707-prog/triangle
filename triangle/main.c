9d73d0fd23cc79bef4ae90e8224837fdb59ef727
sanaa <Sanaakumar07@gmail.com>
2025-09-13 13:01:02 +0530

    Initial Commit

---
+++ triangle/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  triangle
+//
+//  Created by Sanaa Kumar on 13/09/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
